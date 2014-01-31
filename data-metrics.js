b6a8ee1af8e26a9b30166784da706e69dba63a6a
Marlos Carmo <marlos@gmail.com>
2014-01-31 08:29:29 -0200

    Block a href

--- data-metrics.js
+++ data-metrics.js
@@ -2,32 +2,45 @@ function DataMetrics() {}
 
 DataMetrics.prototype = {
   init: function() {
-    this.getItems()
+    this.getItems();
   },
 
   getItems: function() {
     var self = this;
 
-    $('form[data-metrics]').submit(function(e) {
+    $('*[data-metrics]').click(function(e) {
       if (!$(this).hasClass('sending-metrics')) {
-        e.preventDefault();
-
-        $(this).addClass('sending-metrics');
         self.dispatchEvent($(this));
+        $(this).addClass('sending-metrics');
+        return false;
+      } else {
+        if ($(this).attr('href')) {
+          window.location.href = $(this).attr('href');
+        }
       }
     });
 
-    $('*[data-metrics]').click(function() {
+    $('form[data-metrics]').submit(function(e) {
       self.dispatchEvent($(this));
+      
+      if (!$(this).hasClass('sending-metrics')) {
+        return false;
+      }
     });
   },
 
   dispatchEvent: function(elem) {
     var params = elem.attr('data-metrics').split('|');
     window.ga('send', 'event', params[0], params[1], params[2], { 'hitCallback': function() {
+      if (elem.prop('tagName') === 'A') {
+        elem.click();
+      }
+
       if (elem.prop('tagName') === 'FORM') {
         elem.submit();
       }
+
+      elem.removeClass('sending-metrics');
     }});
   }
 };
