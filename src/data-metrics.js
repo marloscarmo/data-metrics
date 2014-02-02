57853157d43aa15320e766c04ff3335a4f5da3a5
Marlos Carmo <marlos@gmail.com>
2014-02-01 22:18:32 -0200

    Removing jQuery dependency

--- src/data-metrics.js
+++ src/data-metrics.js
@@ -1,44 +1,59 @@
+'use strict';
+
 function DataMetrics() {}
 
 DataMetrics.prototype = {
   init: function() {
-    this.getItems();
+    this.addEventListeners();
   },
 
-  getItems: function() {
+  addEventListeners: function() {
     var self = this;
-
-    $('*[data-metrics]').not('form[data-metrics]').click(function() {
-      if (!$(this).hasClass('sending-metrics')) {
-        self.dispatchEvent($(this));
-        $(this).addClass('sending-metrics');
-        return false;
+    var elements = document.querySelectorAll('*[data-metrics]');
+    
+    for (var i = 0; i < elements.length; i++) {
+      if ( elements[i].tagName !== 'FORM' ) {
+        elements[i].onclick = function(e) {
+          self.appendEvent.call(self, e);
+        };
+      } else {
+        elements[i].onsubmit = function(e) {
+          self.appendEvent.call(self, e);
+        };
       }
-    });
+    }
+  },
 
-    $('form[data-metrics]').submit(function() {
-      if (!$(this).hasClass('sending-metrics')) {
-        self.dispatchEvent($(this));
-        $(this).addClass('sending-metrics');
-        return false;
-      }
-    });
+  appendEvent: function (e) {
+    var elem = e.currentTarget;
+    
+    if (!elem.classList.contains('sending-metrics')) {
+      this.dispatchEvent(elem);
+      elem.classList.add('sending-metrics');
+      return false;
+    }
   },
 
   dispatchEvent: function(elem) {
-    var params = elem.attr('data-metrics').split('|');
+    var self = this;
+    var params = elem.getAttribute('data-metrics').split('|');
+    
     window.ga('send', 'event', params[0], params[1], params[2], { 'hitCallback': function() {
-      if (elem.attr('href')) {
-        window.location.href = elem.attr('href');
-      }
-
-      if (elem.prop('tagName') === 'FORM') {
-        elem.submit();
-      }
-
-      elem.removeClass('sending-metrics');
+      self.onEventIsDispatched.call(self, elem);
     }});
+  },
+
+  onEventIsDispatched: function (elem) {
+    if (elem.getAttribute('href')) {
+      window.location.href = elem.getAttribute('href');
+    }
+
+    if (elem.tagName === 'FORM') {
+      elem.submit();
+    }
+
+    elem.classList.remove('sending-metrics');
   }
 };
 
-var dataMetrics = new DataMetrics().init();
+new DataMetrics().init();
