a99c7a2a71580f3f833160b26d1795ea6f440907
Marlos Carmo <marlos@gmail.com>
2014-02-02 08:29:34 -0200

    Finishing example

--- src/data-metrics.js
+++ src/data-metrics.js
@@ -1,59 +1,68 @@
 'use strict';
 
-function DataMetrics() {}
+(function(root) {
+  function DataMetrics() {}
 
-DataMetrics.prototype = {
-  init: function() {
-    this.addEventListeners();
-  },
+  DataMetrics.prototype = {
+    init: function() {
+      this.addEventListeners();
+    },
 
-  addEventListeners: function() {
-    var self = this;
-    var elements = document.querySelectorAll('*[data-metrics]');
-    
-    for (var i = 0; i < elements.length; i++) {
-      if ( elements[i].tagName !== 'FORM' ) {
-        elements[i].onclick = function(e) {
-          self.appendEvent.call(self, e);
-        };
-      } else {
-        elements[i].onsubmit = function(e) {
-          self.appendEvent.call(self, e);
-        };
+    addEventListeners: function() {
+      var self = this;
+      var elements = document.querySelectorAll('*[data-metrics]');
+      
+      for (var i = 0; i < elements.length; i++) {
+        if ( elements[i].tagName !== 'FORM' ) {
+          elements[i].onclick = function(e) {
+            self.appendEvent.call(self, e);
+          };
+        } else {
+          elements[i].onsubmit = function(e) {
+            self.appendEvent.call(self, e);
+          };
+        }
       }
+    },
+
+    appendEvent: function (e) {
+      var elem = e.currentTarget;
+      
+      if (!elem.classList.contains('sending-metrics')) {
+        var params = elem.getAttribute('data-metrics').split('|');
+
+        if (params[1] === undefined) params[1] = null;
+        if (params[2] === undefined) params[2] = null;
+
+        this.sendToGA(params[0], params[1], params[2], elem);
+        elem.classList.add('sending-metrics');
+        return false;
+      }
+    },
+
+    sendToGA: function(category, action, label, elem) {
+      var self = this;
+
+      window.ga('send', 'event', category, action, label, { 'hitCallback': function() {
+        if (elem) {
+          self.onEventIsDispatched.call(self, elem);
+        }
+      }});
+    },
+
+    onEventIsDispatched: function (elem) {
+      if (elem.getAttribute('href')) {
+        window.location.href = elem.getAttribute('href');
+      } else if (elem.tagName === 'FORM') {
+        elem.submit();
+      }
+
+      elem.classList.remove('sending-metrics');
     }
-  },
+  };
 
-  appendEvent: function (e) {
-    var elem = e.currentTarget;
-    
-    if (!elem.classList.contains('sending-metrics')) {
-      this.dispatchEvent(elem);
-      elem.classList.add('sending-metrics');
-      return false;
-    }
-  },
+  root.dataMetrics = new DataMetrics();
+  root.dataMetrics.init();
 
-  dispatchEvent: function(elem) {
-    var self = this;
-    var params = elem.getAttribute('data-metrics').split('|');
-    
-    window.ga('send', 'event', params[0], params[1], params[2], { 'hitCallback': function() {
-      self.onEventIsDispatched.call(self, elem);
-    }});
-  },
+})(this);
 
-  onEventIsDispatched: function (elem) {
-    if (elem.getAttribute('href')) {
-      window.location.href = elem.getAttribute('href');
-    }
-
-    if (elem.tagName === 'FORM') {
-      elem.submit();
-    }
-
-    elem.classList.remove('sending-metrics');
-  }
-};
-
-new DataMetrics().init();
